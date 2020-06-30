270f44d02ed103082dd8a9ec6666ceb4bf2fa1df
unknown <ibnutriyuono@gmail.com>
2020-06-30 19:49:57 +0700

    adding bottom nav

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'white'}}>
+          <Text> XD </Text>
+        </View>
+        <View
+          style={{
+            height: 54,
+            flexDirection: 'row',
+            backfaceVisibility: 'center',
+          }}>
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./assets/icon/home-active.png')}
+            />
+            <Text>Home</Text>
+          </View>
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./assets/icon/order.png')}
+            />
+            <Text>Orders</Text>
+          </View>
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./assets/icon/help.png')}
+            />
+            <Text>Help</Text>
+          </View>
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./assets/icon/inbox.png')}
+            />
+            <Text>Inbox</Text>
+          </View>
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./assets/icon/account.png')}
+            />
+            <Text>Account</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
